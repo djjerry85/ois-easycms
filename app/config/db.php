5c5de22ba29b8eca4e56ca281e0f5d1298bb470f
Jerry <dj_jerry@ukr.net>
2016-02-26 11:32:31 +0200

    first commit

---
+++ app/config/db.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=easycms',
+    'username' => 'easycms',
+    'password' => 'qazxcdews',
+    'charset' => 'utf8',
+    'tablePrefix' => '',
+    'enableSchemaCache' => false,
+];
